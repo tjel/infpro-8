4d3ce2eef25ae3564bdb9a79d5924962fd68184c
rvnlord <rvnlord@gmail.com>
2015-10-14 16:50:38 +0200

    General fixes and optimizations.

--- area/Area/Area.cpp
+++ area/Area/Area.cpp
@@ -1,15 +1,43 @@
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	double PI = 3.1415; // variables can be initialized during declaration 
-	int r;
-	cout << "Enter the radius: \n";
-	cin >> r;
-	cout << "Area of the circle =" << PI * r * r << "\n\n";
+	setlocale(LC_ALL, "");
+
+	string readLine = "";
+	bool isParsable;
+
+	do
+	{
+		double PI = 3.1415; 
+		int r = 0;
+		cout << "Enter the radius: \n";
+		getline(cin, readLine);
+
+		try
+		{
+			r = stoi(readLine);
+			isParsable = true;
+		}
+		catch (...)
+		{
+			isParsable = false;
+		}
+
+		if (isParsable)
+		{
+			cout << "Area of the circle = " << PI * r * r << "\n\n";
+		}
+		else
+		{
+			cout << "Entered Value is not a number\n";
+		}
+	} 
+	while (!isParsable);
 
 	return 0;
 }
