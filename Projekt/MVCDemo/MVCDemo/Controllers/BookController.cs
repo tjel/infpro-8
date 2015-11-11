ac6edf10f3a4ee6acb6cd756be05940022c4de3f
rvnlord <rvnlord@gmail.com>
2015-11-11 17:31:33 +0100

    Search Engine Update
    - Added Search Engine server background
    - Minor fixes and improvements
    - Added database testing App

---
+++ Projekt/MVCDemo/MVCDemo/Controllers/BookController.cs
@@ -0,0 +1,136 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class BookController : BaseController
+    {
+        public ActionResult Index(Search search)
+        {
+            if (!ModelState.IsValid)
+                throw new Exception("Model dla 'search' jest nieprawidłowy");
+
+            var requestType = System.Web.HttpContext.Current.Request.HttpMethod;
+            var searchOptions = new Search { HowMuchTake = 12 };
+
+            var dictDefaultSearchOptions = searchOptions.ToDictionary();
+            var dictSessionSearchOptions = GetSearchParamsSession();
+            var dictPostedSearchOptions = new Dictionary<string, object>();
+
+            if (requestType == "POST")
+            {
+                dictPostedSearchOptions.Add("searchTerm", search.SearchTerm);
+                dictPostedSearchOptions.Add("includeAuthor", search.IncludeAuthor);
+            }
+
+            var dictMergedSearchOptions = dictDefaultSearchOptions;
+            if (dictSessionSearchOptions != null && requestType == "GET")
+                dictMergedSearchOptions = MergeDictonaries(dictMergedSearchOptions, dictSessionSearchOptions);
+            if (requestType == "POST")
+                dictMergedSearchOptions = MergeDictonaries(dictMergedSearchOptions, dictPostedSearchOptions);
+
+            SaveSearchParamsSession(dictMergedSearchOptions);
+            return View();
+        }
+
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        public PartialViewResult GetSearchOptions()
+        {
+            var search = new Search { HowMuchTake = 12 };
+
+            var dictSearchParams = GetSearchParamsSession();
+            if (dictSearchParams != null && dictSearchParams.Count > 0)
+            {
+                search = new Search(dictSearchParams);
+            }
+
+            return PartialView("_SearchOptions", search);
+        }
+
+        public JsonResult GetSearchResults(Search search)
+        {
+            if (!ModelState.IsValid)
+                throw new Exception("Model dla 'search' jest nieprawidłowy");
+
+            var books = GetBooks(search);
+
+            var db = new ProjectDbContext();
+            var users = db.Users.ToList();
+
+            foreach (var b in books)
+                b.Author = users.Single(u => u.Id == b.AuthorId);
+
+            SaveSearchParamsSession(search.ToDictionary());
+
+            if (books.Count == 0)
+            {
+                return Json(new
+                {
+                    ResultsCount = books.Count,
+                    PartialView = string.Empty
+                }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new
+            {
+                ResultsCount = books.Count,
+                PartialView = RenderPartialView("_SearchResults", books)
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetScrollSearchResults(Search search, string scrollDirection)
+        {
+            if (!ModelState.IsValid || !new[] {"scrollup", "scrolldown"}.Contains(scrollDirection.ToLower()))
+                throw new Exception("Model dla 'search' lub Kierunek sortowania przekazany przez Ajax jest nieprawidłowy");
+
+            var sessSearch = new Search((Dictionary<string, object>)Session["SearchParams"]);
+            var expectedSearch = new Search(search) { HowMuchSkip = sessSearch.HowMuchSkip };
+
+            if (Session["SearchParams"] == null || !sessSearch.Equals(expectedSearch))
+                throw new Exception("Sesja jest pusta lub nieprawidłowa");
+
+            var totalSkip = search.HowMuchSkip + sessSearch.HowMuchSkip;
+            search.HowMuchSkip = totalSkip;
+            var searchWithoutInvertedValues = new Search(search);
+
+            switch (scrollDirection.ToLower())
+            {
+                case "scrolldown":
+                    totalSkip = search.HowMuchSkip + search.HowMuchTake - 2;
+                    search.HowMuchSkip = totalSkip;
+                    break;
+                case "scrollup":
+                    totalSkip = search.HowMuchSkip; // dummy, do usunięcia
+                    search.HowMuchSkip = totalSkip;
+                    break;
+                default:
+                    throw new Exception("Nie poprawny kierunek przewijania. ");
+            }
+
+            search.HowMuchTake = 2;
+            var books = GetBooks(search);
+
+            if (books.Count == 0)
+                return Json(new
+                {
+                    ResultsCount = books.Count,
+                    PartialView = string.Empty
+                }, JsonRequestBehavior.AllowGet);
+
+            SaveSearchParamsSession(searchWithoutInvertedValues.ToDictionary());
+
+            return Json(new
+            {
+                ResultsCount = books.Count,
+                PartialView = RenderPartialView("_SearchScrollResults", books)
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Projekt/test/test EF z MySQL/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using ConsoleApplication1.Models;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new ProjectDbContext();
+            var books = db.Books.ToList();
+            var users = db.Users.ToList();
+            foreach (var b in books)
+            {
+                Console.WriteLine(b.Id + "\n" 
+                    + b.Title + "\n" 
+                    + b.Category + "\n" 
+                    + b.AdditionDate + "\n" 
+                    + users.Single(u => u.Id == b.AuthorId).UserName + "\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
